b2f19e4f0477199598863c86c3a648e9db46e417
Riccardo <Bordon>
2019-08-07 22:32:00 +0200

    New version

--- main.py
+++ main.py
@@ -6,13 +6,74 @@ import base64
 import zlib
 
 
-def find_value(i, a4, a5, a7):
-    k = int(round(i))
+def find_value(it, a4, a5, a7):
+    h = int(round(it))
     if a4 == 0 or a5 == 0 or a7 == 0:
-        k -= 2
-    if k % 2 == 0:
-        k += 1
-    return k
+        h -= 2
+    if h % 2 == 0:
+        return h + 1
+    return h
+
+
+def fix_data(k, i, iter_t):
+    data = [round(k * 1.6), round((k << 3) / 3.0), round(k * 3.2), round(k * (704.0 / 165.0)), round(k * 4.8),
+            round(k * (968.0 / 165.0)), round(k * (24673.0 / 3300.0)), k << 3]
+    lpos = 0
+    inte = 6
+    for l in range(8):
+        pos = data[l] + i
+        inte += const * (pos - lpos)
+        lpos = pos
+        res4 = (pos - 4) % 8
+        res5 = (pos - 5) % 8
+        res7 = (pos - 7) % 8
+        j = find_value(inte, res4, res5, res7)
+        if j % k != 0:
+            intep = inte + const
+            jp = find_value(intep, (res4 + 1) % 8, (res5 + 1) % 8, (res7 + 1) % 8)
+            intel = inte - const
+            jl = find_value(intel, (res4 - 1) % 8, (res5 - 1) % 8, (res7 - 1) % 8)
+            if jl % k == 0:
+                data[l] -= 1
+            elif jp % k == 0:
+                data[l] += 1
+            else:
+                st = True
+                sp = int(round(k / 100.0)) - 3
+                intep += (sp - 1) * const
+                res4p = (res4 + sp) % 8
+                res5p = (res5 + sp) % 8
+                res7p = (res7 + sp) % 8
+                intel -= (sp - 1) * const
+                res4l = (res4 - sp) % 8
+                res5l = (res5 - sp) % 8
+                res7l = (res7 - sp) % 8
+                while st:
+                    jp = find_value(intep, res4p, res5p, res7p)
+                    jl = find_value(intel, res4l, res5l, res7l)
+                    if jl % k == 0:
+                        st = False
+                        data[l] -= sp
+                    elif jp % k == 0:
+                        st = False
+                        data[l] += sp
+                    else:
+                        sp += 1
+                        intep += const
+                        res4p = (res4p + 1) % 8
+                        res5p = (res5p + 1) % 8
+                        res7p = (res7p + 1) % 8
+                        intel -= const
+                        res4l = (res4l - 1) % 8
+                        res5l = (res5l - 1) % 8
+                        res7l = (res7l - 1) % 8
+                    iter_t[0] += 1
+        iter_t[0] += 1
+        try:
+            a[data[l] + i] = 1
+        except IndexError:
+            l = 8
+    return data
 
 
 if __name__ == "__main__":
@@ -22,14 +83,14 @@ if __name__ == "__main__":
         u = int(sys.argv[1])
     except IndexError:
         u = 100000
+    n = int((u << 2) / 15.0) + 1
 
     sys.stdout.write("Array... "), sys.stdout.flush()
-    n = int((u << 2)/15.0) + 1
     a = bitarray.bitarray(n)
     a.setall(0)
     sys.stdout.write("done\n")
 
-    iter_tot = 0
+    iter_tot = [0]
     start = time.time()
 
     s7, i7 = 0, 1
@@ -41,9 +102,9 @@ if __name__ == "__main__":
     s29, i29 = 6, 1
     s31, i31 = 7, 1
 
-    sys.stdout.write("7, 11, 13, 17, 23, 29, 31... "), sys.stdout.flush()
+    sys.stdout.write("7, 11, 13, 17, 19, 23, 29, 31... "), sys.stdout.flush()
     while i7 or i11 or i13 or i17 or i23 or i29 or i31:
-        if i7:                                                                                                                                                                                                                                                                                                                                       
+        if i7:
             try:
                 for i in range(18):
                     a[s7+12] = 1
@@ -163,119 +224,190 @@ if __name__ == "__main__":
             except IndexError:
                 del s31
                 i31 = 0
-        iter_tot += 1
+        iter_tot[0] += 1
     del i7, i11, i13, i17, i19, i23, i29, i31
-    sys.stdout.write("done (%d)\n" % iter_tot)
+    sys.stdout.write("done (%d)\n" % iter_tot[0])
 
     sqtn = int(round(math.sqrt(u)))
     sqt = ((sqtn << 2) / 15.0) - 1
-    
+
     k = 37
-    r4 = 4
-    r5 = 3
-    r7 = 1
-    i = 8
-    il = 0
-    inter = 6
+    data = [60, 99, 119, 158, 178, 217, 277, 296]
+    s = 8 + data[-1]
 
-    while k < sqtn:
-        sys.stdout.write("%d... " % k), sys.stdout.flush()
-        n_iter = 0
-        data = [round(k * 1.6), round((k << 3) / 3.0), round(k * 3.2), round(k * (704.0 / 165.0)), round(k * 4.8),
-                round(k * (968.0 / 165.0)), round(k * (24673.0 / 3300.0)), k << 3]
-        err = True
-        for l in range(8):
-            pos = data[l] + i
-            inte = const*pos + 6
-            res4 = (pos - 4) % 8
-            res5 = (pos - 5) % 8
-            res7 = (pos - 7) % 8
-            j = find_value(inte, res4, res5, res7)
+    k1 = 41
+    data1 = [66, 110, 132, 175, 197, 241, 307, 328]
+    s1 = 9 + data1[-1]
 
-            if j % k != 0:
-                intep = inte + const
-                jp = find_value(intep, (res4 + 1) % 8, (res5 + 1) % 8, (res7 + 1) % 8)
+    k2 = 43
+    data2 = [69, 115, 138, 183, 206, 252, 321, 344]
+    s2 = 10 + data2[-1]
 
-                intel = inte - const
-                jl = find_value(intel, (res4 - 1) % 8, (res5 - 1) % 8, (res7 - 1) % 8)
+    i = 10
+    il = 10
+    inter = 43.49999999999999
+    r4 = 6
+    r5 = 5
+    r7 = 3
 
-                if jl % k == 0:
-                    data[l] -= 1
-                elif jp % k == 0:
-                    data[l] += 1
-                else:
-                    n_iter = 0
-                    st = True
-                    sp = int(round(k/100.0)) - 3 
-                    intep += (sp-1)*const
-                    res4p = (res4 + sp) % 8
-                    res5p = (res5 + sp) % 8
-                    res7p = (res7 + sp) % 8
-
-                    intel -= (sp-1)*const
-                    res4l = (res4 - sp) % 8
-                    res5l = (res5 - sp) % 8
-                    res7l = (res7 - sp) % 8
-
-                    while st:
-                        jp = find_value(intep, res4p, res5p, res7p)
-
-                        jl = find_value(intel, res4l, res5l, res7l)
-
-                        if jl % k == 0:
-                            st = False
-                            data[l] -= sp
-                        elif jp % k == 0:
-                            st = False
-                            data[l] += sp
-                        else:
-                            sp += 1
-                            intep += const
-                            res4p = (res4p + 1) % 8
-                            res5p = (res5p + 1) % 8
-                            res7p = (res7p + 1) % 8
-
-                            intel -= const
-                            res4l = (res4l - 1) % 8
-                            res5l = (res5l - 1) % 8
-                            res7l = (res7l - 1) % 8
-                        n_iter += 1
-            try:
-                a[data[l]+i] = 1
-            except IndexError:
-                err = False
-        
-        if err:
-            s = i + data[7]
-            state = True
-            while state:
-                try:
-                    n_iter += 1
-                    for add in data:
-                        a[s+add] = 1
-                    s += data[-1]
-                except IndexError:
-                    state = False
-
-        i += 1
-        while i < sqt and a[i] != 0:
+    while s != sqtn and s1 != sqtn and s2 != sqtn:
+        try:
+            for add in data:
+                a[s + add] = 1
+            s += data[-1]
+        except IndexError:
             i += 1
-            n_iter += 1
-        if i < sqt:
-            inter += (i-il) * const
-            k = int(round(inter))
-            r4 = (r4 + i - il) % 8
-            r5 = (r5 + i - il) % 8
-            r7 = (r7 + i - il) % 8
-            k = find_value(inter, r4, r5, r7)
-            il = i
-        else:
-            k = sqtn
+            while i < sqt and a[i] != 0:
+                i += 1
+            iter_tot[0] += (i - il)
+            if i < sqt:
+                ko = k
+                inter += (i - il) * const
+                r4 = (r4 + i - il) % 8
+                r5 = (r5 + i - il) % 8
+                r7 = (r7 + i - il) % 8
+                k = find_value(inter, r4, r5, r7)
+                print("%d -> %d" % (ko, k))
+                data = fix_data(k, i, iter_tot)
+                s = i + data[-1]
+                il = i
+            else:
+                s = sqtn
 
-        sys.stdout.write("done (%d)\n" % n_iter)
-        iter_tot += n_iter
+        try:
+            for add in data1:
+                a[s1 + add] = 1
+            s1 += data1[-1]
+        except IndexError:
+            i += 1
+            while i < sqt and a[i] != 0:
+                i += 1
+            iter_tot[0] += (i - il)
+            if i < sqt:
+                ko = k1
+                inter += (i - il) * const
+                r4 = (r4 + i - il) % 8
+                r5 = (r5 + i - il) % 8
+                r7 = (r7 + i - il) % 8
+                k1 = find_value(inter, r4, r5, r7)
+                print("%d -> %d" % (ko, k1))
+                data1 = fix_data(k1, i, iter_tot)
+                s1 = i + data1[-1]
+                il = i
+            else:
+                s1 = sqtn
 
-    print("\n%15d %5.5f %20d\n" % (u, (time.time() - start), iter_tot))
+        try:
+            for add in data2:
+                a[s2 + add] = 1
+            s2 += data2[-1]
+        except IndexError:
+            i += 1
+            while i < sqt and a[i] != 0:
+                i += 1
+            iter_tot[0] += (i - il)
+            if i < sqt:
+                ko = k2
+                inter += (i - il) * const
+                r4 = (r4 + i - il) % 8
+                r5 = (r5 + i - il) % 8
+                r7 = (r7 + i - il) % 8
+                k2 = find_value(inter, r4, r5, r7)
+                print("%d -> %d" % (ko, k2))
+                data2 = fix_data(k2, i, iter_tot)
+                s2 = i + data2[-1]
+                il = i
+            else:
+                s2 = sqtn
+        iter_tot[0] += 1
+
+    if s1 == sqtn:
+        s1 = s2
+        data1 = data2
+    if s == sqtn:
+        s = s2
+        data = data2
+    del s2, data2
+
+    while s != sqtn and s1 != sqtn:
+        try:
+            for add in data:
+                a[s + add] = 1
+            s += data[-1]
+        except IndexError:
+            i += 1
+            while i < sqt and a[i] != 0:
+                i += 1
+            iter_tot[0] += i - il
+            if i < sqt:
+                ko = k
+                inter += (i - il) * const
+                r4 = (r4 + i - il) % 8
+                r5 = (r5 + i - il) % 8
+                r7 = (r7 + i - il) % 8
+                k = find_value(inter, r4, r5, r7)
+                print("%d -> %d" % (ko, k))
+                data = fix_data(k, i, iter_tot)
+                s = i + data[-1]
+                il = i
+            else:
+                s = sqtn
+
+        try:
+            for add in data1:
+                a[s1 + add] = 1
+            s1 += data1[-1]
+        except IndexError:
+            i += 1
+            while i < sqt and a[i] != 0:
+                i += 1
+            iter_tot[0] += (i - il)
+            if i < sqt:
+                ko = k1
+                inter += (i - il) * const
+                r4 = (r4 + i - il) % 8
+                r5 = (r5 + i - il) % 8
+                r7 = (r7 + i - il) % 8
+                k1 = find_value(inter, r4, r5, r7)
+                print("%d -> %d" % (ko, k1))
+                data1 = fix_data(k1, i, iter_tot)
+                s1 = i + data1[-1]
+                il = i
+            else:
+                s1 = sqtn
+        iter_tot[0] += 1
+
+    if s == sqtn:
+        s = s1
+        data = data1
+    del s1, data1
+
+    while s != sqtn:
+        try:
+            for add in data:
+                a[s + add] = 1
+            s += data[-1]
+        except IndexError:
+            i += 1
+            while i < sqt and a[i] != 0:
+                i += 1
+            iter_tot[0] += (i - il)
+            if i < sqt:
+                ko = k
+                inter += (i - il) * const
+                k = int(round(inter))
+                r4 = (r4 + i - il) % 8
+                r5 = (r5 + i - il) % 8
+                r7 = (r7 + i - il) % 8
+                k = find_value(inter, r4, r5, r7)
+                print("%d -> %d" % (ko, k))
+                data = fix_data(k, i, iter_tot)
+                il = i
+            else:
+                s = sqtn
+        iter_tot[0] += 1
+
+    print("\n%15d %5.5f %20d\n" % (u, (time.time() - start), iter_tot[0]))
     del data
 
     sys.stdout.write("Scrittura... "), sys.stdout.flush()
